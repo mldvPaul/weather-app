5bc4e9b82467412b695c6867193af36c51593bc2
mldvPaul <mldvPaul@gmail.com>
2023-09-14 10:24:05 +0200

    APIkey

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import {IoMdSunny, IoMdRainy, IoMdCloudy, IoMdSnow, IoMdThunderstorm, IoMdSearch,} from 'react-icons/io';
+import {BsCloudHaze2, BsCloudDrizzleFill, BsEye, BsWater, BsThermometer, BsWind} from 'react-icons/bs';
+import {TbTemperatureCelsius} from 'react-icons/tb';
+import {ImSpinner8} from 'react-icons/im';
 
+const APIkey = (process.env.REACT_APP_SECRET_KEY);
+console.log(APIkey)
 function App() {
   return (
     <div>
       <h1>weather-app</h1>
+      
     </div>
   );
 }
